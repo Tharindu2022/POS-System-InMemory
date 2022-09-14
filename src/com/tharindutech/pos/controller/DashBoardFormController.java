1fdb3cc4bd0d795965b928d939d83a40d842a7f8
Tharindu <htharindum1988@gmail.com>
2022-09-14 09:35:37 +0530

    Initial Commit

--- src/com/tharindutech/pos/controller/DashBoardFormController.java
+++ src/com/tharindutech/pos/controller/DashBoardFormController.java
@@ -1,10 +1,40 @@
 package com.tharindutech.pos.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashBoardFormController {
-    public void openCustomerFormOnAction(ActionEvent actionEvent) {
+    public AnchorPane dashboardContext;
+    public Label lblDateTime;
+
+    public void initialize (){
+        setDateAndTime();
     }
+
+    private void setDateAndTime() {
+        Timeline time= new Timeline(new KeyFrame(Duration.ZERO,e->{
+            DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+            lblDateTime.setText(LocalDateTime.now().format(formatter));
+        }),new KeyFrame(Duration.seconds(1)));
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+    }
+
+
 
     public void openItemFormOnAction(ActionEvent actionEvent) {
     }
@@ -14,4 +44,15 @@ public class DashBoardFormController {
 
     public void openOrderDetailsFormOnAction(ActionEvent actionEvent) {
     }
+
+    public void openCustomerFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("CustomerForm");
+    }
+    private void setUi(String ui) throws IOException {
+        Stage stage = (Stage) dashboardContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+ui+".fxml"))));
+        stage.centerOnScreen();
+    }
+
+
 }
---
+++ src/com/tharindutech/pos/db/DataBase.java
@@ -0,0 +1,15 @@
+package com.tharindutech.pos.db;
+
+import com.tharindutech.pos.model.Customer;
+
+import java.util.ArrayList;
+
+public class DataBase {
+
+    public static ArrayList<Customer> customerTable= new ArrayList<Customer>();
+
+
+
+
+
+}
