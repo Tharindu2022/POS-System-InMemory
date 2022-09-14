d128fbf182b0aeba6ea689749362f076f2b72d75
Tharindu <htharindum1988@gmail.com>
2022-09-14 10:25:11 +0530

    Adding searchCustomers()

--- src/com/tharindutech/pos/controller/CustomerFormController.java
+++ src/com/tharindutech/pos/controller/CustomerFormController.java
@@ -3,10 +3,13 @@ package com.tharindutech.pos.controller;
 import com.tharindutech.pos.db.DataBase;
 import com.tharindutech.pos.model.Customer;
 import com.tharindutech.pos.view.tm.CustomerTM;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class CustomerFormController {
@@ -16,15 +19,21 @@ public class CustomerFormController {
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+    public TableView tblCustomer;
 
     public  void initialize(){
+        txtId.setText().;
+
         searchCustomers();
     }
 
     private void searchCustomers() {
-        ObservableList<CustomerTM> observableList= new   {
+        ObservableList<CustomerTM> tmList= FXCollections.observableArrayList() ;
+        for (Customer c:DataBase.customerTable) {
+            Button btn= new Button("DELETE");
+            tmList.add(new CustomerTM(c.getId(),c.getName(),c.getAddress(),c.getSalary(),btn));
         }
-
+        tblCustomer.setItems(tmList);
     }
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
